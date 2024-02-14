546eea259380ccfbcb1d6c747371184b8b3fd957
luisa <moraLF20@outlook.com>
2024-02-13 22:32:14 -0700

    modificando clases Cuenta y Cliente

--- netbeans/bancoDominio/src/main/java/banco/bancodominio/Cliente.java
+++ netbeans/bancoDominio/src/main/java/banco/bancodominio/Cliente.java
@@ -4,10 +4,124 @@
  */
 package banco.bancodominio;
 
+import java.sql.Date;
+
 /**
  *
  * @author luiis
  */
 public class Cliente {
+    private int id_cliente;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private Date fecha_nacimiento;
+    private int edad;
+    private int id_domicilio;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id_cliente, String nombre, String apellidoP, String apellidoM, Date fecha_nacimiento, int edad, int id_domicilio) {
+        this.id_cliente = id_cliente;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.edad = edad;
+        this.id_domicilio = id_domicilio;
+    }
+
+    public Cliente(String nombre, String apellidoP, String apellidoM, Date fecha_nacimiento, int edad, int id_domicilio) {
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.edad = edad;
+        this.id_domicilio = id_domicilio;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getId_domicilio() {
+        return id_domicilio;
+    }
+
+    public void setId_domicilio(int id_domicilio) {
+        this.id_domicilio = id_domicilio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.id_cliente;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return this.id_cliente == other.id_cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id_cliente=" + id_cliente + ", nombre=" + nombre + ", apellidoP=" + apellidoP + ", apellidoM=" + apellidoM + ", fecha_nacimiento=" + fecha_nacimiento + ", edad=" + edad + ", id_domicilio=" + id_domicilio + '}';
+    }
     
 }
--- netbeans/bancoDominio/src/main/java/banco/bancodominio/Cuenta.java
+++ netbeans/bancoDominio/src/main/java/banco/bancodominio/Cuenta.java
@@ -4,10 +4,86 @@
  */
 package banco.bancodominio;
 
+import java.sql.Date;
+import java.util.Objects;
+
 /**
  *
  * @author luiis
  */
 public class Cuenta {
+    private String num_cuenta;
+    private String estado;
+    private float saldo;
+    private Date fecha_creacion;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String num_cuenta, String estado, float saldo, Date fecha_creacion) {
+        this.num_cuenta = num_cuenta;
+        this.estado = estado;
+        this.saldo = saldo;
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public String getNum_cuenta() {
+        return num_cuenta;
+    }
+
+    public void setNum_cuenta(String num_cuenta) {
+        this.num_cuenta = num_cuenta;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public Date getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(Date fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.num_cuenta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cuenta other = (Cuenta) obj;
+        return Objects.equals(this.num_cuenta, other.num_cuenta);
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" + "num_cuenta=" + num_cuenta + ", estado=" + estado + ", saldo=" + saldo + ", fecha_creacion=" + fecha_creacion + '}';
+    }
     
 }
