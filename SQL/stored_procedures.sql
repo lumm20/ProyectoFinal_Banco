c2761285d3de602576835fa2bd4ec4c2af70ebf0
luisa <moraLF20@outlook.com>
2024-02-17 17:41:27 -0700

    modificando stored procedures

--- SQL/stored_procedures.sql
+++ SQL/stored_procedures.sql
@@ -47,7 +47,7 @@ delimiter ;
 #---------------------------------------------------
 #consultar todas las transferencias
 delimiter $$
-create procedure sp_consultarTransferenciasPorNumCuenta(in numCuenta varchar(10))
+create procedure sp_consultarTransferencias(in numCuenta varchar(10))
 begin
 	select t.id_transaccion, t.num_cuenta, t.fecha_hora, t.monto,
 			tf.num_cuenta_destino as 'destino transferencia'
@@ -77,12 +77,36 @@ begin
 end $$
 delimiter ;
 #---------------------------------------------------
+#procesar una transaccion
+delimiter $$
+create procedure sp_procesarTransaccion(
+	in monto decimal(18,2),
+    in fechaHora datetime,
+    in numCuentaOrigen varchar(10),
+    out idTransaccion int)
+begin 
+#declarando manejador de excepciones
+	declare exit handler for sqlexception
+    begin
+		rollback;
+	end;
+    
+    start transaction;
+    set autocommit=0;
+    #agrega el registro de la transaccion de tipo transferencia
+    insert into transacciones (num_cuenta, fecha_hora, monto)
+    values (numCuentaOrigen, fechaHora, monto);
+    
+    set idTransaccion=last_insert_id();
+    
+    commit;
+end $$
+delimiter ;
+#---------------------------------------------------
 #procesar una transferencia
 delimiter $$
 create procedure sp_procesarTransferencia(
-	in monto float,
-    in fechaHora datetime,
-    in numCuentaOrigen varchar(10),
+	in idTransaccion int,
     in numCuentaDestino varchar(10))
 begin
 #declarando manejador de excepciones
@@ -93,15 +117,15 @@ begin
     
     start transaction;
     set autocommit=0;
-    #agrega el registro de la transaccion de tipo transferencia
-    insert into transacciones (num_cuenta, fecha_hora, monto, tipo_transaccion)
-    values (numCuentaDestino, fechaHora, monto, "transferencia");
-    #guarda el id auto-generado de la transaccion
-    set @idTransaccion=last_insert_id();
+    
     #agrega la transferencia con el id de la transaccion
-    insert into transaccion_transferencia values (numCuentaDestino, @idTransaccion);
+    insert into transaccion_transferencia values (numCuentaDestino, idTransaccion);
+    
+    #actualiza la columna 'tipo_transaccion' en la tabla transacciones en el registro con el id del parametro
+    update transacciones set tipo_transaccion="transferencia" where id_transaccion=idTransaccion;
     
     #----set @saldoCuentaOrigen=(select saldo from cuentas where numero_de_cuenta=numCuentaOrigen);
+    
     #hace el calculo del saldo nuevo de la cuenta origen, es decir, se le resta el monto
     #de la transferencia a su saldo
     call sp_calcularSaldo(numCuentaOrigen, monto,"restar",@saldoNuevo);
@@ -112,6 +136,41 @@ begin
     call sp_calcularSaldo(numCuentaDestino, monto,"sumar",@saldoNuevo);
     #actualiza el saldo de la cuenta origen en el registro de la tabla de cuentas
     update cuentas set saldo=@saldoNuevo where numero_de_cuenta=numCuentaDestino;
+    
+    commit;
+end $$
+delimiter ;
+#---------------------------------------------------
+#procesar un retiro sin cuenta
+delimiter $$
+create procedure sp_procesarRetiro(
+	in idTransaccion int,
+    in folio varchar(32),
+    in estado varchar(10),
+    in contra varchar(8))
+begin
+#declarando manejador de excepciones
+	declare exit handler for sqlexception
+    begin
+		rollback;
+	end;
+    
+    start transaction;
+    set autocommit=0;
+    
+    #agrega el retiro con el id de la transaccion
+    insert into transaccion_sin_cuenta values (idTransaccion, folio, estado,contra);
+    
+    #actualiza la columna 'tipo_transaccion' en la tabla transacciones en el registro con el id del parametro
+    update transacciones set tipo_transaccion="retiro sin cuenta" where id_transaccion=idTransaccion;
+    
+    #----set @saldoCuentaOrigen=(select saldo from cuentas where numero_de_cuenta=numCuentaOrigen);
+    
+    #hace el calculo del saldo nuevo de la cuenta origen, es decir, se le resta el monto
+    #del retiro a su saldo
+    call sp_calcularSaldo(numCuentaOrigen, monto,"restar",@saldoNuevo);
+    #actualiza el saldo de la cuenta origen en el registro de la tabla de cuentas
+    update cuentas set saldo=@saldoNuevo where numero_de_cuenta=numCuentaOrigen;
     
     commit;
 end $$
