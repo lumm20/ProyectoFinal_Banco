b2b2b54291d58479c93aba5317d68763507ac200
luisa <moraLF20@outlook.com>
2024-02-18 21:06:54 -0700

    agregando control presentacion

---
+++ netbeans/bancoPresentacion/src/main/java/banco/bancopresentacion/control/ControlPresentacion.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco.bancopresentacion.control;
+
+import banco.bancopresentacion.*;
+
+/**
+ *
+ * @author luiis
+ */
+public class ControlPresentacion {
+    public ControlPresentacion(){
+    }
+    
+    public void despliegaInicio(){
+        Inicial ini=new Inicial();
+    }
+    public void despliegaLogin(){
+        Login login=new Login();
+    }
+    
+    public void despliegaRegistro(){
+        Registro registro=new Registro();
+    }
+    
+    public void despliegaPrincipal(){
+        Principal principal=new Principal();
+    }
+    
+    public void despliegaRetiroSinCuenta(){
+        Retiro_Sin_Cuenta retiro=new Retiro_Sin_Cuenta();
+    }
+}
